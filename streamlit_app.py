0b705699583bb4feccac90b87c575a098f2b327a
tkamble <trishala.kamble@bitwiseglobal.com>
2024-06-22 00:35:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write(
 from snowflake.snowpark.functions import col
 cnx = st.connection("snowflake")
 session = cnx.session()
-session = get_active_session()
+# session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
