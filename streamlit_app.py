8bd572d0cfa78dd73eb709a4f7009047cf8495a9
tkamble <trishala.kamble@bitwiseglobal.com>
2024-06-22 00:39:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
@@ -10,12 +11,12 @@ st.write(
 )
 
 
-from snowflake.snowpark.functions import col
+
 cnx = st.connection("snowflake")
 session = cnx.session()
-# session = get_active_session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+
 
 
 ingredients_list = st.multiselect(
@@ -23,8 +24,7 @@ ingredients_list = st.multiselect(
     ,my_dataframe
 )
 if ingredients_list : 
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
+
 
     ingredients_string = ''
     for fruit_chosen in ingredients_list : 
