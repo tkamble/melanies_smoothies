839f96cec89bf33e8e895e09912895e70d4c16ff
tkamble <trishala.kamble@bitwiseglobal.com>
2024-06-24 14:37:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,9 +32,9 @@ if ingredients_list :
     ingredients_string = ''
     for fruit_chosen in ingredients_list : 
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        #st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
-        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
 
     my_insert_stmt = """insert into SMOOTHIES.PUBLIC.ORDERS (ingredients,name_on_order)
