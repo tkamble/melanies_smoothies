003611331aa1ce67663fc13b2b161290fa98a6b9
tkamble <trishala.kamble@bitwiseglobal.com>
2024-06-24 14:32:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 # Import python packages
 from snowflake.snowpark.functions import col
 import streamlit as st
+import requests
 
 # Write directly to the app
 st.title("Customize your smoothie :cup_with_straw:")
@@ -31,9 +32,9 @@ if ingredients_list :
     ingredients_string = ''
     for fruit_chosen in ingredients_list : 
         ingredients_string += fruit_chosen + ' '
-        #st.subheader(fruit_chosen + 'Nutrition Information')
-        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
-        #fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     st.write(ingredients_string)
 
     my_insert_stmt = """insert into SMOOTHIES.PUBLIC.ORDERS (ingredients,name_on_order)
@@ -46,7 +47,4 @@ if ingredients_list :
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+ name_on_order+'!', icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
